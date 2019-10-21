c6b7d1addca9c063953b82d89af03e569d3291e8
GabrielleSudik <gabrielle.b.sudik@gmail.com>
2019-10-21 18:58:59 -0400

    complete through lesson four

--- Ch03/03_02/Begin/Adapter/Program.cs
+++ Ch03/03_02/Begin/Adapter/Program.cs
@@ -9,6 +9,13 @@ using System.Collections.Generic;
 namespace Adapter.Demonstration
 {
     // 'Adaptee' class
+
+    //start here with the class,
+    //just a list of employees.
+    //pretend this is a class from a third party org that
+    //our company works with.
+    //their code might not be compatible with ours.
+    //but we will make it so.
     class ThirdPartyEmployee
     {
         public List<string> GetEmployeeList()
@@ -29,6 +36,9 @@ namespace Adapter.Demonstration
     }
 
     // 'Adapter' class
+    //inherits both the class and the interfaces from above.
+    //the method comes from the interface
+    //the thing returned is the method in the class.
     class EmployeeAdapter : ThirdPartyEmployee, ITarget
     {
         public List<string> GetEmployees()
@@ -37,12 +47,41 @@ namespace Adapter.Demonstration
         }
     }
 
+    //that's really it.
+    //you have some class. in this case our class 
+    //instantiates and populates a list of employees.
+    //then you have an interface. in this case it will "getEmployees()"
+    //and finally you put them together by implementing the interface
+    //and activating the other class inside it.
+
+    //why call it an adapter?
+    //because it can "adapt" to handle different things in different classes.
+    //eg, one class uses a list, another uses an array.
+    //prof gives no example, though :\
+
     // 'Client' class
     class Client
     {
         static void Main(string[] args)
         {
-          // TODO
+          //how will the Client use that adapter, then?
+          
+            Console.WriteLine("Employee list from 3rd party org:");
+            Console.WriteLine();
+
+            //use the ITarget interface to call the
+            //class you need -- ie, ThirdPartyEmployee
+
+            //first create an instance of the thing that will
+            //work with both our I and the outside class:
+            ITarget adapter = new EmployeeAdapter();
+
+            //then use its info and method to iterate through the list.
+            foreach (string employee in adapter.GetEmployees())
+            {
+                Console.WriteLine(employee);
+            }
+            Console.ReadLine();
         }
     }
 }
--- Ch04/04_02/Begin/Composite/Program.cs
+++ Ch04/04_02/Begin/Composite/Program.cs
@@ -9,6 +9,8 @@ using System.Collections.Generic;
 namespace Composite.Demonstration
 {
     //'IComponent' interface
+    //3 properties, 1 method.
+    //will be implemented by plain employees and supervisors.
     public interface IEmployee
     {
         int EmployeeID { get; set; }
@@ -17,6 +19,8 @@ namespace Composite.Demonstration
         void PerformanceSummary();
     }
     //'Leaf' class - will be leaf node in tree structure
+    //this is for employees who do not supervise anyone
+    //they can see only their own records.
     public class Employee : IEmployee
     {
         public int EmployeeID { get; set; }
@@ -30,6 +34,9 @@ namespace Composite.Demonstration
         }
     }
     //'Composite' class - will be branch node in tree structure
+    //a supervisor who can see her own records and the
+    //records of her staff.
+    //notice the extra methods to list staff and add staff.
     public class Supervisor : IEmployee
     {
         public int EmployeeID { get; set; }
@@ -54,7 +61,38 @@ namespace Composite.Demonstration
     {
         static void Main(string[] args)
         {
-            // TODO
+            //start with creating some employees:
+            Employee lucy = new Employee {EmployeeID = 1,
+                Name = "Lucy", Rating = 4};
+            Employee ricky = new Employee {EmployeeID = 2,
+                Name = "Ricky", Rating = 3};
+            Employee ginger = new Employee {EmployeeID = 3,
+                Name = "Ginger", Rating = 5};
+            
+            //and supervisors:
+            Supervisor thurston = new Supervisor {EmployeeID = 4,
+                Name = "Thurston", Rating = 4};
+            Supervisor donna = new Supervisor {EmployeeID = 5,
+                Name = "Donna", Rating = 5};
+
+            //create their supervisor/employee relationships:
+            thurston.AddSubordinate(lucy);
+            donna.AddSubordinate(ricky);
+            donna.AddSubordinate(ginger);
+
+            Console.WriteLine("Employee can see their performance summary:");
+            lucy.PerformanceSummary();
+
+            Console.WriteLine("Supervisors can see their subordinates:");
+            donna.PerformanceSummary();
+
+            Console.WriteLine("Subordinate performaces:");
+            foreach (Employee employee in donna.ListSubordinates)
+	        {
+                employee.PerformanceSummary();
+	        }
+
+            Console.ReadLine();
         }
     }
 }
