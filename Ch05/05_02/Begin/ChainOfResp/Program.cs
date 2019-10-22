930e164836423c7e4742f154c9f04d75cfbf4f66
GabrielleSudik <gabrielle.b.sudik@gmail.com>
2019-10-21 20:20:53 -0400

    complete through lesson five

--- Ch05/05_02/Begin/ChainOfResp/Program.cs
+++ Ch05/05_02/Begin/ChainOfResp/Program.cs
@@ -15,13 +15,46 @@ namespace Chain.Demonstration
     {        
         static void Main()
         {
-            // TODO
+            //this code comes last after the blueprint for the chain is set up below.
+            
+            //define some Approvers:
+            Approver lucy = new Director();
+            Approver ricky = new VicePresident();
+            Approver ginger = new President();
+
+            //assign each of their successors (except Pres, who is the end of the chain):
+            lucy.SetSuccessor(ricky);
+            ricky.SetSuccessor(ginger);
+
+            //generate a purchase request:
+            Purchase p1 = new Purchase(888, 350.00, "Assets");
+            //then send it to the first link in the approval chain:
+            lucy.ProcessRequest(p1);
+            //approved by director
+
+            //repeat with other amounts:
+            Purchase p2 = new Purchase(889, 40000.00, "Vehicle");
+            lucy.ProcessRequest(p2);
+            //approved by president
+
+            Purchase p3 = new Purchase(890, 140000.00, "Office Annex");
+            lucy.ProcessRequest(p3);
+            //not approved; meeting scheduled.
+
+            //wait for user:
+            Console.ReadKey();
+
         }
     }
 
     /// <summary>
     /// The 'Handler' abstract class
     /// </summary>
+    //We name it Approver because pretend you are an employee
+    //who needs approval to, say, make a big purchase.
+    //could be an interface if we wanted.
+    //basically outlines the successor and a method stub
+    //for what the actual approver will actually do.
     abstract class Approver
     {
         protected Approver successor;
@@ -37,6 +70,9 @@ namespace Chain.Demonstration
     /// <summary>
     /// The 'ConcreteHandler' class
     /// </summary>
+    //in practice, we want the director to be able to approve
+    //up to $10k. if he can't, the request will pass on to the
+    //successor (ie, the next person in line)
     class Director : Approver
     {
         public override void ProcessRequest(Purchase purchase)
@@ -56,6 +92,7 @@ namespace Chain.Demonstration
     /// <summary>
     /// The 'ConcreteHandler' class
     /// </summary>
+    //Ditto with the VP but up to $25k
     class VicePresident : Approver
     {
         public override void ProcessRequest(Purchase purchase)
@@ -75,6 +112,9 @@ namespace Chain.Demonstration
     /// <summary>
     /// The 'ConcreteHandler' class
     /// </summary>
+    //Finally, up to the President, up to $100k.
+    //In this case, there is no successor; the chain
+    //"stops" with a decision to hold a meeting.
     class President : Approver
     {
         public override void ProcessRequest(Purchase purchase)
@@ -96,6 +136,8 @@ namespace Chain.Demonstration
     /// <summary>
     /// Class holding request details
     /// </summary>
+    //This is just the request with its pertinent details.
+    //it's what gets passed to each approver.
     class Purchase
     {
         private int _number;
